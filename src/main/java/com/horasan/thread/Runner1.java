c7b539c6b79b7873035bb15906300336590dcd33
Horasan, Riza <rizahorasan@gmail.com>
2021-03-03 22:33:48 +0100

    thread classes with sleep() method

--- src/main/java/com/horasan/thread/Runner1.java
+++ src/main/java/com/horasan/thread/Runner1.java
@@ -4,6 +4,14 @@ public class Runner1 extends Thread{
 	@Override
 	public void run() {
 		for(int a = 0; a < 10; a++) {
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 			System.out.println("Runner1: " + a);
 		}
 	}
--- src/main/java/com/horasan/thread/Runner2.java
+++ src/main/java/com/horasan/thread/Runner2.java
@@ -4,6 +4,14 @@ public class Runner2 extends Thread{
 	@Override
 	public void run() {
 		for(int a = 0; a < 10; a++) {
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 			System.out.println("Runner2: " + a);
 		}
 	}

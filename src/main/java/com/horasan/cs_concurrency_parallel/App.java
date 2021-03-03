cd4a50d72f3f2cb5dfad46d1f43a900b1bb7fae0
Horasan, Riza <rizahorasan@gmail.com>
2021-03-03 21:59:37 +0100

    project is created.

---
+++ src/main/java/com/horasan/cs_concurrency_parallel/App.java
@@ -0,0 +1,13 @@
+package com.horasan.cs_concurrency_parallel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
